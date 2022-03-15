f158e86037e37f39996b83b46a04d284cff30730
Alessio Napoli <alessionapoli99@icloud.com>
2022-03-15 13:59:33 +0100

    Ended Exercise

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     public $name;
     public $filmProductionCompany;
     public $language;
+    public $genres;
     public $vote;
     public $cast;
   
@@ -10,43 +11,72 @@
       $this -> name = $_name;
       $this -> filmProductionCompany = $_company;
     }
-    public function getPrintVote($_vote){
+    public function getVote($_vote){
       if(is_numeric($_vote)){
         $this -> vote = $_vote;
-        echo "<li>" . $this -> vote . "</li>";
       }else{
-        echo "<li>Is not a Number</li>";
+        $this -> vote = "Is not a Number";
       }
     }
-    public function getPrintCast(...$actor){
+    public function setVote(){
+      echo "<li>" . $this -> vote . "</li>";
+    }
+    public function getGenre(...$_genres){
+      $this -> genres = $_genres;
+    }
+    public function setGenre(){
+      foreach($this -> genres as $genre){
+        echo "<li>" . $genre . "</li>";
+      }
+    }
+    public function getCast(...$actor){
       $this -> cast = $actor;
+    }
+    public function setCast(){
       foreach($this -> cast as $person){
         echo "<li>" . $person . "</li>";
       }
     }
   }
 
+//  New Instance Object
   $avengers = new Movies("Avengers: EndGame", "Marvel");
   echo "<h2>" . $avengers -> name . "</h2>";
   echo "<ul>";
     echo "<li>" . $avengers -> filmProductionCompany . "</li>";
     echo "<li>" . $avengers -> language = "En" . "</li>";
-    $avengers -> getPrintVote(8);
-    echo "<li>Cast</li>";
+    echo "<li>Genres:</li>";
+    $avengers -> getGenre("Action", "SuperHero", "Science Fiction", "Adventure", "Fantasy");
       echo "<ul>";
-      $avengers -> getPrintCast("Robert Downey Jr.","Tom Holland","Chris Evans","Scarlett Johansson");
+      $avengers -> setGenre();
+      echo "</ul>";
+    $avengers -> getVote(8);
+    $avengers -> setVote();
+    echo "<li>Cast:</li>";
+    $avengers -> getCast("Robert Downey Jr.","Tom Holland","Chris Evans","Scarlett Johansson");
+      echo "<ul>";
+      $avengers -> setCast();
       echo "</ul>";
   echo "</ul>";
 
+
+//  New Instance Object
   $batman = new Movies("The Batman", "DC Film");
   echo "<h2>" . $batman -> name . "</h2>";
   echo "<ul>";
     echo "<li>" . $batman -> filmProductionCompany . "</li>";
     echo "<li>" . $batman -> language = "En" . "</li>";
-    $batman -> getPrintVote(7);
-    echo "<li>Cast</li>";
+    echo "<li>Genres:</li>";
+    $avengers -> getGenre("Action", "Drama", "SuperHero", "Adventure", "Crime Film", "Mystery");
       echo "<ul>";
-      $batman -> getPrintCast("Robert Pattinson","Zoë Kravitz","Paul Dano","Colin Farrell");
+      $avengers -> setGenre();
+      echo "</ul>";
+    $batman -> getVote(7);
+    $batman -> setVote();
+    echo "<li>Cast</li>";
+    $batman -> getCast("Robert Pattinson","Zoë Kravitz","Paul Dano","Colin Farrell");
+      echo "<ul>";
+      $batman -> setCast();
       echo "</ul>";
   echo "</ul>";
 
